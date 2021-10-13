6bce18defb2f9bae4f0adf9446c800bbc842b463
Kirill Vasiltsov <interalia.dev@gmail.com>
2021-10-14 01:16:31 +0900

    deploy

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use glitch::glitch_replace;
 use glitch::glitch_sort;
 use lambda_http::handler;
+use lambda_http::Body;
 use lambda_http::{
     http::{HeaderValue, StatusCode},
     IntoResponse, Request, RequestExt, Response,
 };
 use lambda_runtime::{self, Context, Error};
 
+use jemallocator;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
-use std::fs;
+// use std::fs;
 
-/* #[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc; */
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-/* async fn apply_glitch(req: Request, _c: Context) -> Result<impl IntoResponse, Error> {
+async fn apply_glitch(mut req: Request, _c: Context) -> Result<impl IntoResponse, Error> {
     let payload = req.body_mut();
     match payload {
-        Body::Binary(image) => {}
+        Body::Binary(image) => {
+            glitch(image);
+            Ok(image.to_owned())
+        }
+        _ => unimplemented!(),
     }
-    let data = payload.ok_or("No body!")?; // Check if body exists.
-    Ok("0")
-} */
+}
 
 fn glitch(image: &mut [u8]) {
     glitch_replace(image);
@@ -35,11 +39,11 @@ fn glitch(image: &mut [u8]) {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let path = std::env::args().nth(1).unwrap_or_else(|| panic!("whoops"));
+    /* let path = std::env::args().nth(1).unwrap_or_else(|| panic!("whoops"));
     let mut image = fs::read(path)?;
 
-    fs::write("glitched.jpg", image)?;
-    /* let func = handler(find_index);
-    lambda_runtime::run(func).await?; */
+    fs::write("glitched.jpg", image)?; */
+    let func = handler(apply_glitch);
+    lambda_runtime::run(func).await?;
     Ok(())
 }
